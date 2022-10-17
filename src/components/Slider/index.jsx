e83f6c5bb3f7cd86026f943c62951f250ee87ac1
Dam Nam <nam.dam@tda.company>
2022-10-17 09:52:45 +0700

    feat: BannerSlider

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,59 +1,55 @@
 import React from "react";
-import { ArrowBackIosNewOutlined, ArrowForwardIosOutlined } from "@mui/icons-material";
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import styles from "./styles.module.css";
 import slider from "../../constants/slider";
+import Carousel from "react-material-ui-carousel";
 
 const Banner = () => {
+  const defaultSettings = {
+    autoPlay: true,
+    animation: "fade",
+    indicators: true,
+    duration: 500,
+    navButtonsAlwaysVisible: false,
+    navButtonsAlwaysInvisible: false,
+    cycleNavigation: true,
+    fullHeightHover: true,
+    swipe: true,
+  };
   return (
     <div>
-      <Box component="section" className={styles.slider_section}>
-        <Box className={styles.owl_stage_outer}>
-          {slider.map(({ title, title2, content, desc }, key) => (
-            <Grid key={key} className={styles.owl_item}>
-              <Box className={styles.single_slider}>
-                <Box className={styles.slider_content}>
-                  <Typography variant="span">{content}</Typography>
-                  <Typography variant="h1">{title}</Typography>
-                  <Typography variant="h1">{title2}</Typography>
-                  <Typography variant="p" className={desc}>
-                    {desc}
-                  </Typography>
-                  <br></br>
-                  <Typography
-                    component="a"
-                    href="#"
-                    target="_blank"
-                    rel="noreferrer"
-                    variant="button"
-                  >
-                    Discover Now
-                  </Typography>
-                </Box>
-              </Box>
-            </Grid>
-          ))}
-        </Box>
-        <Box className={styles.owl_nav}>
-          <Box className={styles.owl_prev}>
-            <ArrowBackIosNewOutlined fontSize="large" />
-          </Box>
-          <Box className={styles.owl_next}>
-            <ArrowForwardIosOutlined fontSize="large" />
-          </Box>
-        </Box>
-        <Box className={styles.owl_dots}>
-          {slider.map(({ name: title, items }, key) => (
-            <Grid key={key} className={styles.owl_dot}>
-              <Typography className="" variant="span">
-                .
-              </Typography>
-            </Grid>
-          ))}
-        </Box>
-      </Box>
+      <Carousel className="BannerSlider" {...defaultSettings}>
+        {slider.map((item, key) => {
+          return <Project item={item} key={key} />;
+        })}
+      </Carousel>
     </div>
   );
 };
-
+const Project = ({ item }) => {
+  return (
+    <Box
+      component="section"
+      className={styles.slider_section}
+      sx={{ background: `url(${item.image})` }}
+    >
+      <Box className={styles.owl_stage_outer}>
+        <Box className={styles.single_slider}>
+          <Box className={styles.slider_content}>
+            <Typography variant="span">{item.content}</Typography>
+            <Typography variant="h1">{item.title}</Typography>
+            <Typography variant="h1">{item.title2}</Typography>
+            <Typography variant="p" className={styles.desc}>
+              {item.desc}
+            </Typography>
+            <br></br>
+            <Typography component="a" href="#" target="_blank" rel="noreferrer" variant="button">
+              Discover Now
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
 export default Banner;
